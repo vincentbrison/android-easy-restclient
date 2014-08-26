5f133e8020ea7357af22e5fc9f760dd20cb5ee5c
Vincent Brison <vincent.brison@gmail.com>
2014-08-26 16:37:09 +0200

    Refactor

--- lib/src/main/java/com/vb/openlibraries/easyrestclient/lib/WebService.java
+++ lib/src/main/java/com/vb/openlibraries/easyrestclient/lib/WebService.java
@@ -6,13 +6,13 @@ import java.util.Map;
  * Created by Vincent Brison on 10/08/2014.
  */
 public abstract class WebService {
-    public enum METHOD_NAME{GET, POST}
-    public enum PROTOCOL{HTTP, HTTPS}
+    public enum METHOD_NAME{GET, POST, PUT, DELETE}
+    public enum SCHEME{HTTP, HTTPS}
 
     public abstract String getBaseURL();
     public abstract String getPathURL();
     public abstract METHOD_NAME getMethodName();
-    public abstract PROTOCOL getProtocol();
+    public abstract SCHEME getProtocol();
 
     protected Map<String, String> args;
 
